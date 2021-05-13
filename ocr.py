de36d6c6d74d53fdad0a32be7956d81f7993688a
Rosian Pop <poprosian@gmail.com>
2021-05-14 01:08:13 +0300

    Added video + readme changes

--- ocr.py
+++ ocr.py
@@ -2,14 +2,15 @@ from cv2 import cv2
 import pytesseract
 from PIL import Image
 
-
 def ocr_image(path):
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     img = cv2.medianBlur(img, 3)
     cv2.imwrite(path, img)
 
-    txt = pytesseract.image_to_string(Image.open(path))
+    new_img = Image.open(path)
+    txt = pytesseract.image_to_string(new_img)
     print()
     print(txt)
     print()
